067aeb3ca6e01f1c066251c3ee191ed34ad92d5c
Alessandro <Alessandro@LAPTOP-TTLDS6O6.station>
2020-05-29 13:11:03 +0200

    assa

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,65 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
-
-    @FXML // ResourceBundle that was given to the FXMLLoader
+	Dictionary d=new Dictionary();
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private Button btnLingua;
+
+    @FXML
+    private TextField txtTesto;
+
+    @FXML
+    private Button btnSpell;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    void doClear(ActionEvent event) {
+    	txtResult.clear();
+    	txtTesto.clear();
+
+    }
+
+    @FXML
+    void doLingua(ActionEvent event) {
+    	
+    	
+
+    }
+
+    @FXML
+    void doSpell(ActionEvent event) {
+    	
+
+    }
+
+    @FXML
     void initialize() {
+        assert btnLingua != null : "fx:id=\"btnLingua\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTesto != null : "fx:id=\"txtTesto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnSpell != null : "fx:id=\"btnSpell\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
-
-
